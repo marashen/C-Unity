849e0b2190712cbefbcb01a59e6f117e6806112a
marashen <j-byrd.colon@outlook.com>
2022-01-04 08:47:24 -0500

    Car Insurance Assignment

---
+++ BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+Console.WriteLine("Age?");
+int age = 35;
+Console.WriteLine(age);
+bool dui = false;
+Console.WriteLine("Have you ever had a DUI?");
+Console.WriteLine(dui);
+Console.WriteLine("Tickets?");
+int tickets = 0;
+Console.WriteLine(tickets);
+Console.WriteLine("Qualified?");
+bool quals = (age >= 21 && tickets < 2 && !dui);
+Console.WriteLine(quals);
