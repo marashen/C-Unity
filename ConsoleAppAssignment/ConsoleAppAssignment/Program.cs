ae6c33654806a31a9efcac8ed097185d41bbe95f
marashen <j-byrd.colon@outlook.com>
2022-01-04 09:28:15 -0500

    Loops

---
+++ ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+double num1 = 45.45;
+while (num1 > 1)
+{
+    Console.WriteLine(num1);
+    num1 = num1 / 2;
+}
+int num2 = 112;
+do
+{
+    Console.WriteLine(num2);
+    num2++;
+}
+while (num2 < 115);
