b88283b7b34a579a93c22ff3f5efb9993222aeb1
marashen <j-byrd.colon@outlook.com>
2022-01-04 20:48:46 -0500

    Added User Input

--- BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
+++ BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
@@ -7,16 +7,38 @@ namespace Boolin
         static void Main(string[] args)
         {
             Console.WriteLine("Age?");
-            int age = 35;
+            string ageIn = Console.ReadLine();
+            int age = Convert.ToInt32(ageIn);
             Console.WriteLine(age);
-            bool dui = false;
-            Console.WriteLine("Have you ever had a DUI?");
-            Console.WriteLine(dui);
-            Console.WriteLine("Tickets?");
-            int tickets = 0;
+            int looper = 1;
+            while (looper == 1)
+            {
+                Console.WriteLine("Have you ever had a DUI? Please respond yes or no.");
+                string duiIn = Console.ReadLine();
+                if (duiIn == "yes")
+                {
+                    bool dui = true;
+                    Console.WriteLine(dui);
+                    looper -= 1;
+                }
+                else if (duiIn == "no")
+                {
+                    bool dui = false;
+                    Console.WriteLine(dui);
+                    looper -= 1;
+                }
+                else
+                {
+                    Console.WriteLine("please enter 'yes' or 'no'");
+                }
+            }
+            Console.WriteLine("Number of Tickets?");
+            string tickIn = Console.ReadLine();
+            int tickets = Convert.ToInt32(tickIn);
+            Console.WriteLine(age);
             Console.WriteLine(tickets);
             Console.WriteLine("Qualified?");
-            bool quals = (age >= 21 && tickets < 2 && !dui);
+            bool quals = (age >= 21 && tickets < 2);
             Console.WriteLine(quals);
         }
     }
