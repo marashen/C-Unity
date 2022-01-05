ec4aa88922b1bff7e01b2696bb8e5b3d8a7e27d9
marashen <j-byrd.colon@outlook.com>
2022-01-04 21:27:07 -0500

    More Methods

---
+++ MoreMethods/MethMath.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+   public class ManyMews
+    {
+        public int x;
+        public int y;
+        public void DoMath()
+        {
+            x += 4;
+            Console.WriteLine(y);
+        }
+    }
---
+++ MoreMethods/MoreMethods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+
+
+namespace MoreMethods
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            ManyMews newNum = new ManyMews();
+
+            newNum.x = 6;
+            newNum.y = 8;
+
+            newNum.DoMath();
+        }
+    }
+}
