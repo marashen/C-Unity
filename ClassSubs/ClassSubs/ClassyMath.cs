7111d97f05b575f0f2401298ba3a56f8016e71a2
marashen <j-byrd.colon@outlook.com>
2022-01-08 17:39:15 -0500

    More Class Submissions

---
+++ ClassSubs/ClassSubs/ClassyMath.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClassSubs
+{
+    public class ClassyMath
+    {
+        int x;
+        public void method1(int x)
+        {
+            x = x / 2;
+            Console.WriteLine("Your number divided by 2: " + x);
+        }
+    }
+}
---
+++ ClassSubs/ClassSubs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassSubs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassyMath vclassy = new ClassyMath();
+            Console.WriteLine("Choose a number!");
+            int userN = Convert.ToInt32(Console.ReadLine());
+            vclassy.method1(userN);
+        }
+    }
+}
