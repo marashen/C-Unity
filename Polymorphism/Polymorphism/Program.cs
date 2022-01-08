787b5d0b81f8a974caa0387419a07e19560a54e6
marashen <j-byrd.colon@outlook.com>
2022-01-08 18:17:45 -0500

    Poly Assignment

---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuittable sVar;
+            sVar = new Employee();
+            sVar.Quit();
+        }
+    }
+}
---
+++ Polymorphism/Polymorphism/interfacets.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Polymorphism
+{
+    public abstract class Person
+    {
+        string fName;
+        string lName;
+        public abstract void SayName(string fName, string lName);
+    }
+    public class Employee : IQuittable
+    {
+        public void Quit()
+        {
+            Console.WriteLine("Yes");
+        }
+    }
+    interface IQuittable
+    {
+        void Quit(); 
+    }
+}
