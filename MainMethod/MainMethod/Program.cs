2541fc78a2d6c85d25084e6062a3bbd8454450e7
marashen <j-byrd.colon@outlook.com>
2022-01-08 16:34:48 -0500

    Main Method Fixes

---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods userIn = new Methods();
+            int num1 = 115;
+            userIn.mainmethod1(num1);
+            float num2 = 4.5f;
+            userIn.mainmethod2(num2);
+            string num3 = "5";
+            userIn.mainmethod3(num3);
+        }
+    }
+}
---
+++ MainMethod/MathClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace MainMethod
+{
+    public class Methods
+    {
+        int x;
+        float y;
+        int stringN;
+        public void mainmethod1(int x)
+        {
+            x += 73;
+            Console.WriteLine(x);
+        }
+        public void mainmethod2(float y)
+        {
+            y += 216.45f;
+            Console.WriteLine(y);
+        }
+        public void mainmethod3(string z)
+        {
+            Console.WriteLine("Choose a number!");
+            z = Console.ReadLine();
+            int stringN = Convert.ToInt32(z);
+            stringN += 42;
+            Console.WriteLine("Your number + 42 = " + stringN);
+        }
+    }
+}
