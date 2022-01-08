de8d2d3f12729465ea37e2b81d8d5676d8040f02
marashen <j-byrd.colon@outlook.com>
2022-01-08 15:19:11 -0500

    Console App 6 Specifics

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> moons = new List<string>(7);
+            List<string> moons2 = new List<string>(7);
+            moons.Add("Luna");
+            moons.Add("Titan");
+            moons.Add("Ganymede");
+            moons.Add("Luna");
+            moons.Add("Phobos");
+            moons.Add("Deimos");
+            moons.Add("Io");
+            foreach (string i in moons)
+            {
+                Console.WriteLine("-    " + i);
+                bool duped = moons2.Any(s => s.Contains(i));
+                if (duped == false)
+                {
+                    Console.WriteLine("No Repeats");
+                }
+                else if (duped == true)
+                {
+                    Console.WriteLine("The Moon " + i + " is repeated twice!");
+                }
+                moons2.Add(i);
+            }
+        }
+    }
+}
