71e76acd3489f94a1ef74074024359554da3f9f0
marashen <j-byrd.colon@outlook.com>
2022-01-08 18:32:01 -0500

    Overloading Assignment

---
+++ OperationOverload/OperationOverload/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OperationOverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operator oper = new Operator(20, 20);
+            oper = -oper;
+        }
+    }
+    class Operator
+    {
+        int num1;
+        int num2;
+        public Operator(int num3, int num4)
+        {
+            num1 = num3;
+            num2 = num4;
+        }
+        public static Operator operator -(Operator operations)
+        {
+            operations.num1 = -operations.num1;
+            operations.num2 = -operations.num2;
+            return operations;
+        }
+    } 
+}
