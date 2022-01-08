fc7aaff29ad2a42cd8fe2679d2555e884557bcfc
marashen <j-byrd.colon@outlook.com>
2022-01-08 18:52:12 -0500

    Parameters

---
+++ Parameters/Paralax.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Parameters
+{
+    public abstract class Person
+    {
+        string fName;
+        string lName;
+        public abstract void SayName(string fName, string lName);
+    }
+    public class Employee<Typists>
+    {
+        public Typists things { get; set; }
+    }
+}
+    
---
+++ Parameters/Parameters/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Parameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> types = new Employee<string>();
+            types.things = "1 string";
+            types.things = "2 string";
+            types.things = "red string";
+            types.things = "blue string";
+            Employee<int> type5 = new Employee<int>();
+            type5.things = 1;
+            type5.things = 2;
+            foreach (char i in types.things)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
