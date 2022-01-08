a417b136a6402567248dc51fb8be6d90dfbc8c9c
marashen <j-byrd.colon@outlook.com>
2022-01-08 18:09:00 -0500

    Abstract Class Assignment

---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AbstractClass
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Person sVar;
+            sVar = new Employee();
+            string firstN = "sample";
+            string lastN = "student";
+            sVar.SayName(firstN, lastN);
+        }
+    }
+}
---
+++ AbstractClass/abstractical.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace AbstractClass
+{
+    public abstract class Person
+    {
+        string fName;
+        string lName;
+        public abstract void SayName(string fName, string lName);
+    }
+    public class Employee : Person
+    {
+        public override void SayName(string fName, string lName)
+        {
+            Console.WriteLine(fName + " " + lName);
+        }
+    }
+}
