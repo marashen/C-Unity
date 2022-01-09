b921078803deaba3706a2d5ec7dd121b4a6527d2
marashen <j-byrd.colon@outlook.com>
2022-01-08 22:43:51 -0500

    Datetime submissions

---
+++ DatetimeSubs/DatetimeSubs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DatetimeSubs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime();
+            Console.WriteLine(dt);
+            var dateT4 = "1/08/2022";
+
+            var dateT1 = DateTime.TryParse(dateT4, out dt);
+            if (dateT1)
+            {
+                Console.WriteLine(dt);
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+            Console.WriteLine("Choose a number!");
+            int userDate = Convert.ToInt32(Console.ReadLine());
+             DateTime dt8 = new DateTime(2022, 1, 8, 10, 41, 43);
+            TimeSpan dt9 = new TimeSpan(0, userDate, userDate, userDate);
+            Console.WriteLine(dt8 + dt9);
+        }
+    }
+}
---
+++ MartianCasino/MartianCasino/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mars!");
+            Thread.Sleep(3000);
+            Console.Write("Do you wish to begin onboarding? Please enter yes or no! ");
+            string init = Console.ReadLine();
+            Thread.Sleep(1000);
+            if (init != "yes" || init != "Yes")
+            {
+                Console.WriteLine("Then lets begin!");
+                Thread.Sleep(2000);
+                Console.Write("What is your name? ");
+                string playerName = Console.ReadLine();
+                Console.WriteLine("Actually..");
+                Thread.Sleep(1500);
+                Console.WriteLine("How much money are you willing to wager on a quick game?");
+                Thread.Sleep(750);
+                Console.Write("Please enter a whole number value!");
+                int bank = Convert.ToInt32(Console.ReadLine());
+                Thread.Sleep(1000);
+                Console.WriteLine("Alright " + playerName + ", I hope you're ready to lose all $" + bank + " dollars!");
+                Player player = new Player(playerName, bank);
+                Game game = new TwentyOneGame();
+                game += player;
+                player.isActivelyPlaying = true;
+                while (player.isActivelyPlaying == true && player.Balance > 0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Seeyah!");
+            }
+            else
+            {
+                Console.WriteLine("Well, there is nothing else to do! Goodbye!");
+            }
+
+        }
+    }
+}
