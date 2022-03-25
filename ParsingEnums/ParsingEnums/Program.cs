0719e7417427c9b6b7dd2bf5171e3eadaaa9c68a
marashen <j-byrd.colon@outlook.com>
2022-03-24 20:57:28 -0400

    Enum Fixes

--- ParsingEnums/ParsingEnums/Program.cs
+++ ParsingEnums/ParsingEnums/Program.cs
@@ -16,9 +16,23 @@ namespace ParsingEnums
         }
         static void Main(string[] args)
         {
-                Console.WriteLine("Please choose a day of the week!");
+            Console.WriteLine("Please choose a day of the week!");
+
+            bool isWeek;
+
+            try
+            {
                 string weekV = Console.ReadLine();
+                if (weekV.Length >= 6)
+                {
+                    isWeek = true;
+                }                
                 Weeks weekly = (Weeks)Weeks.Parse(typeof(Weeks), weekV);
+            }
+            catch (Exception)
+            {
+                isWeek = false;
+            }
         }
     }
 }
